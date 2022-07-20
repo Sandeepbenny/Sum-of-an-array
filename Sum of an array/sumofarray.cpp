05d0cef6689b00ce97f32f84cbcbc7c6276bdad8
Sandeep Benny <sanduchackonal@gmail.com>
2022-07-21 02:19:39 +0530

    Subtraction and Addition operation has been added

--- Sum of an array/sumofarray.cpp
+++ Sum of an array/sumofarray.cpp
@@ -6,12 +6,16 @@
 #include<vector>
 #include<algorithm>
 #include<cmath>
-void sum(int arr[],int n)
+void operation(int arr[],int n)
 {
-            int sum=0;
+            int sum=0,sub=0;
             for(int i=0;i<n;i++)
                 {
                     sum+=arr[i];
+                }
+            for(int i=0;i<n;i++)
+                {
+                    sub-=arr[i];
                 }
             for(int i=0;i<n;i++)
                {
@@ -19,28 +23,36 @@ void sum(int arr[],int n)
                 std::cout<<arr[i];
                 std::cout<<" +";
                }
-            std::cout<<"\nThe sum is \n";
-            std::cout<<"\n";
+            std::cout<<"\nThe sum is :\n";
             std::cout<<sum;
             std::cout<<"\n";
+            for(int i=0;i<n;i++)
+               {
+                std::cout<<"\n";
+                std::cout<<arr[i];
+                std::cout<<" -";
+               }
+            std::cout<<"\nThe sub is: \n";
+            std::cout<<sub;
+            std::cout<<"\n";
+        
         }
 int main()
-    {
+        {
         system("cls");
         int arr[10],n;
-        std::cout<<"Enter the Size of the array \n";
+        system("cls");
+        std::cout<<"\n   "<<std::endl;
+        std::cout<<"\n  "<<std::endl;
+        std::cout<<"\t ** OPERATION ON AN, ARRAY ALGORITHM BY SANDEEP*** \n"<<std::endl;
+        std::cout<<" \n Enter the Size of the array \n";
         std::cin>>n;
         std::cout<<" \n Enter the array element you want to add \n ";
         for(int i=0;i<n;i++)
             {
                 std::cin>>arr[i];
             }
-        std::cout<<" \n \tDisplaying the array \n";
-        for(int i=0;i<n;i++)
-            {
-                std::cout<<arr[i];   
-                std::cout<<"\n";  
-               }
-     sum(arr,n);
-     getch();
+        system("cls");                
+        operation(arr,n);
+        getch();
     }
